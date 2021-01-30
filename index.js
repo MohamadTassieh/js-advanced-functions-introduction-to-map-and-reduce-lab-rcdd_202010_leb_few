e4fee0330323e881018f8f8b3e7ea069644f5d97
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-30 18:51:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ mapToNegativize(sourceArray)
 mapToNoChange(sourceArray)
 mapToDouble(sourceArray)
 mapToSquare(sourceArray)
+
+reduceToTotal(sourceArray, startingPoint)
+reduceToAllTrue(sourceArray)
+reduceToAnyTrue(sourceArray)
