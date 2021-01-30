e63d037b82737f707bf0d43ed244bec55b52ef00
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-30 18:56:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,45 +4,46 @@ function mapToNegativize(src) {
   for (let i = 0; i < src.length; i++ ) {
     r.push(-1 * src[i])
   }
-  return r
+  return r;
 }
 function mapToNoChange(src) {
   let r = []
   for (let i = 0; i < src.length; i++ ) {
     r.push(src[i])
   }
-  return r
+  return r;
 }
 function mapToDouble(src) {
   let r = []
   for (let i = 0; i < src.length; i++ ) {
     r.push(2 * src[i])
   }
-  return r
+  return r;
 }
 function mapToSquare(src) {
   let r = []
   for (let i = 0; i < src.length; i++ ) {
     r.push(src[i] * src[i])
   }
-  return r
+  return r;
 }
 function reduceToTotal(src, startingPoint=0) {
   let total = startingPoint
   for (let i = 0; i < src.length; i++ ) {
     total = total + src[i]
   }
-  return total
+  return total;
 }
 function reduceToAllTrue(src) {
   for (let i = 0; i < src.length; i++ ) {
-    if (!src[i]) return false
+    if (!src[i]) 
+    return false;
   }
-  return true
+  return true;
 }
 function reduceToAnyTrue(src) {
   for (let i = 0; i < src.length; i++ ) {
-    if (src[i]) return true
+    if (src[i]) return true;
   }
-  return false
+  return false;
 }
